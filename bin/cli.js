f36fbc2e8a6e907384037ab422da6a7928a1d312
nikeshwarpandey <nikeshwarpandey@gmail.com>
2025-04-01 21:35:23 +0530

    Initial commit for latest-cli

---
+++ bin/cli.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+const {program} = require('commander');
+
+//command 1: greet
+program
+    .command('greet <name>')
+    .description('Greet a person')
+    .action((name) => { 
+        console.log(`Hello, ${name}!`);
+    })
+
+//command 2: add
+program
+    .command('add <num1> <num2>')
+    .description('Add two numbers')
+    .action((num1, num2) => {
+        const sum = parseFloat(num1) + parseFloat(num2);
+        console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+    });
+
+    //Parse the arguments and run the commands
+    program.parse(process.argv);
+    
